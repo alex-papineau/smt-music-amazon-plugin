815ffe3e6dd9c19ccecc3f8ad34519cd715471ad
@ <99501899+alex-papineau@users.noreply.github.com>
2026-02-23 23:39:00 -0600

    feat: Add offscreen HTML page for audio playback and update the plugin's toast header text.

--- Content.js
+++ Content.js
@@ -31,7 +31,7 @@ function showSamuraiToast() {
   const toast = document.createElement('div');
   toast.id = 'smt4-toast';
   toast.innerHTML = `
-    <div class="toast-header">SMT IV: Amazon Edition</div>
+    <div class="toast-header">Amazon SMT Music Companion</div>
     <div class="toast-body">Oh, a Hunter...</div>
   `;
   document.body.appendChild(toast);
